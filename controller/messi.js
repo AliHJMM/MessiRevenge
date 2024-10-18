31138739a88b369d307459167fde996561f039f9
Ali <AliHasanJMM@gmail.com>
2024-10-18 12:21:06 +0300

    Func to set keybinds

--- controller/messi.js
+++ controller/messi.js
@@ -13,3 +13,26 @@ const windowWidth = window.innerWidth;
 let leftPosition = messi.offsetLeft;
 let speed = 10;
 let ballInterval = null;
+
+// Sets up event listeners for keydown and keyup to control Messi's movement and shooting
+function setupControls() {
+    document.addEventListener("keydown", (e) => {
+      e.preventDefault();
+      if (e.key === "ArrowRight" || e.key === "d") keys.right = true;
+      if (e.key === "ArrowLeft" || e.key === "a") keys.left = true;
+      if ((e.key === "ArrowUp" || e.key === "w" || e.key === " ") && isAllowed) {
+        keys.up = true;
+        startShooting();
+      }
+    });
+  
+    document.addEventListener("keyup", (e) => {
+      e.preventDefault();
+      if (e.key === "ArrowRight" || e.key === "d") keys.right = false;
+      if (e.key === "ArrowLeft" || e.key === "a") keys.left = false;
+      if (e.key === "ArrowUp" || e.key === "w" || e.key === " ") {
+        keys.up = false;
+        stopShooting();
+      }
+    });
+  }
