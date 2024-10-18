f6403d792edf8c87fcf868666390f818b8e4e019
Ali <AliHasanJMM@gmail.com>
2024-10-18 12:26:39 +0300

    Func to handle victory

--- model/endGameHandler.js
+++ model/endGameHandler.js
@@ -40,3 +40,40 @@ export function gameOver() {
     window.location.reload(true);
   };
 }
+
+// Handles the victory state, displaying victory UI and stopping game elements
+export function victory() {
+    if (dialogDisplayed) return;
+    dialogDisplayed = true;
+  
+    const victoryImage =
+      "https://res.cloudinary.com/dovvtjnrs/image/upload/v1728493376/messiwon_aptp5u.jpg";
+    const victoryElement = document.getElementById("victory");
+    victoryElement.style.backgroundImage = `url('${victoryImage}')`;
+  
+    victoryElement.style.visibility = "visible";
+    document.getElementById("restart-button").style.display = "block";
+    document.getElementById("messi").style.display = "none";
+    document.getElementById("scoreboard").style.display = "none";
+  
+    const madridContainers = document.getElementsByClassName("madrid-container");
+    if (madridContainers.length > 0) {
+      madridContainers[0].style.display = "none";
+    }
+  
+    freezeRonaldos();
+    freezeTimer();
+    clearInterval(ronaldoInterval);
+    stopAllBackgroundMusic();
+  
+    showEndStoryDialog(true);
+  
+     // Define the restart function to reset the game
+    window.restartGame = () => {
+      victoryElement.style.display = "none";
+      document.getElementById("restart-button").style.display = "none";
+      dialogDisplayed = false;
+      window.location.reload(true);
+    };
+  }
+  
