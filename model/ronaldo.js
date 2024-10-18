3ded1240abba4748e75d65e3d1bc2ebbb4dcce2b
Ali <AliHasanJMM@gmail.com>
2024-10-18 12:28:35 +0300

    Func to Create Ronaldo

--- model/ronaldo.js
+++ model/ronaldo.js
@@ -8,3 +8,49 @@ let ronaldoMovementInterval = 100;
 export function setRonaldoMovementParameters(newMovementInterval) {
   ronaldoMovementInterval = newMovementInterval;
 }
+
+// Creates a new Ronaldo element originating from a random alive Madrid element
+export function createRonaldo() {
+    const aliveMadrid = [];
+    madridArray.forEach((row) => {
+      row.forEach((madrid) => {
+        if (madrid.status === "alive") {
+          aliveMadrid.push(madrid);
+        }
+      });
+    });
+  
+    if (aliveMadrid.length === 0) return;
+  
+    const randomMadrid =
+      aliveMadrid[Math.floor(Math.random() * aliveMadrid.length)];
+    const ronaldo = document.createElement("img");
+    ronaldo.src =
+      "https://res.cloudinary.com/dovvtjnrs/image/upload/v1728493345/HHHHHHHHHHHHHHHHHHHHH-removebg-preview_znergl.png";
+    ronaldo.style.height = "50px";
+    ronaldo.classList.add("ronaldo");
+    ronaldo.style.position = "absolute";
+    ronaldo.style.left = `${randomMadrid.x + 25}px`;
+    ronaldo.style.top = `${randomMadrid.y + 50}px`;
+    document.body.insertBefore(ronaldo, document.body.firstChild);
+  
+    let topPosition = parseInt(ronaldo.style.top, 10);
+  
+    // Moves Ronaldo element downward and checks for collisions with Messi
+    function moveRonaldo() {
+      topPosition += 10;
+      ronaldo.style.top = `${topPosition}px`;
+  
+      detectClash(ronaldo, messi);
+  
+      if (topPosition > window.innerHeight) {
+        ronaldo.remove();
+        activeRonaldos.splice(activeRonaldos.indexOf(moveRonaldo), 1);
+      }else {
+           requestAnimationFrame(moveRonaldo);
+      }
+    }
+  
+    requestAnimationFrame(moveRonaldo);
+    activeRonaldos.push(moveRonaldo);
+  }
